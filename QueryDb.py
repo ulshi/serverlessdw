95bcea7bb794ef4a426fcccde26e3b005c6e4675
ulshi <46480421+ulshi@users.noreply.github.com>
2021-11-07 02:36:48 +0800

    Update QueryDb.py
    Add tool class and modify the translate method

--- QueryDb.py
+++ QueryDb.py
@@ -2,20 +2,21 @@
 from collections import defaultdict
 from typing import List
 
+
 class QueryDb:
+    """
+    Provide several methods to execute database query
+    """
     def __init__(self):
         self.table = defaultdict(dict)
+        self.tool = Dbtool()
 
-
-    def create(self, table_name: str, col_num: int, col_names: List, keys: List):
+    def create(self, table_name: str, col_names: List, keys: List):
         """create table"""
         if table_name not in self.table:
-            self.table[table_name] = {'values': [], 'col_num': col_num, 'col_name': col_names, 'keys': keys}
+            self.table[table_name] = {'values': [], 'col_name': col_names, 'keys': keys}
 
-
-    def insert(self, table_name: str, values: List) -> None:
-        table_keys = self.table[table_name]['keys']
-        keys_index_list = [1, 2]
+    def insert(self, table_name: str, insert_values: List) -> None:
         table_values = self.table[table_name]['values']
         table_cols = self.table[table_name]['col_name']
         table_keys = self.table[table_name]['keys']
@@ -23,13 +24,12 @@ class QueryDb:
             cur = 0
             tmp_keys = []
             for pos, val in enumerate(table_keys):
-                if table_cols[pos] == values[pos]:
+                if insert_values[pos] == values[pos]:
                     cur += 1
                     tmp_keys.append(val)
             if cur == len(table_keys):
-                return tmp_keys
-        self.table[table_name]['values'].append(values)
-
+                return
+        self.table[table_name]['values'].append(insert_values)
 
     def select(self, table_name: str, conditions: List) -> List:
         table_values = self.table[table_name]['values']
@@ -40,14 +40,14 @@ class QueryDb:
         for index, col_name in enumerate(table_cols):
             col_index[col_name] = index
         for condition in conditions:
-            col_name, col_val = map(str, condition.split('=').stripe())
+            col_name, col_val = self.tool.remove_space(condition, '=')
             if col_name not in table_cols:
                 res = [col_name, 'Not exist']
                 return res
         for condition in conditions:
             cur = 0
             tmp_res = []
-            col_name, col_val = map(str, condition.split('=').stripe())
+            col_name, col_val = self.tool.remove_space(condition, '=')
             for value in table_values:
                 if cur == 0:
                     if str(value[col_index[col_name]]) == str(col_val):
@@ -62,42 +62,73 @@ class QueryDb:
         return res
 
 
+class Dbtool:
+    """
+    Provide several Db tools
+    """
+    def __init__(self):
+        pass
+
+    def remove_space(self, simple, symbol=''):
+        if symbol != '':
+            return [i.strip() for i in simple.split(symbol)]
+        return [i.strip() for i in simple.split()]
+
+
+
 class Parse:
+    """
+    Provide several methods to parse SQL statement to command which QueryDb class need
+    """
     def __init__(self):
         self.query = ''
-
+        self.tool = Dbtool()
 
     def translate(self, query):
         self.query = query
-        com, value = self.query.lower().split(':')
-        # '''table_name, col_num, col_names, keys'''
+        com, value = self.tool.remove_space(self.query.lower(), ':')
+        # '''table_name, col_names, keys'''
         if com.strip() == 'create':
-            table_name, col_num, col_names_str, keys_str = value.split(',')
-            col_names = [i.strip() for i in col_names_str.split(',')]
-            keys = [i.strip() for i in keys_str.split(',')]
-            return [table_name, int(col_num), col_names, keys]
+            table_name, col_names_str, keys_str = value.split(',')
+            col_names = self.tool.remove_space(col_names_str)
+            keys = self.tool.remove_space(keys_str)
+            return [table_name, col_names, keys]
         # '''table_name, values'''
         if com.strip() == 'insert':
             table_name, values_str = value.split(',')
-            values = [i.strip() for i in values_str.split(' ')]
+            values = self.tool.remove_space(values_str)
             return [table_name, values]
         # '''table_name, conditions'''
         if com.strip() == 'select':
-            table_name, conditions_str = value.split(',')
-            conditions = [i.strip() for i in conditions_str.split('and')]
+            table_name, conditions_str = self.tool.remove_space(value, ',')
+            conditions = self.tool.remove_space(conditions_str, 'and')
             return [table_name, conditions]
 
 
-
-
 if __name__ == '__main__':
-        parse = Parse()
-        select = parse.translate('create: behavior, 1, a, a')
-        db = QueryDb()
-        db.create(*select)
-        insert = parse.translate('insert: behavior, 1')
-        db.insert(*insert)
-        db.insert(*insert)
-        select = parse.translate('select: behavior, a=1')
-        db.select(*select)
-        print('success')
+    num = 3
+    parse = Parse()
+    select = parse.translate('create: behavior, a, a')
+    db = QueryDb()
+    db.create(*select)
+    insert = parse.translate('insert: behavior, 1')
+    db.insert(*insert)
+    db.insert(*insert)
+    select = parse.translate('select: behavior, a=1')
+    print(db.select(*select))
+    print('success1')
+    select = parse.translate('create: behavior_d, a b, a')
+    db.create(*select)
+    insert = parse.translate('insert: behavior_d, 1 5')
+    db.insert(*insert)
+    insert = parse.translate('insert: behavior_d, 1 2')
+    db.insert(*insert)
+    insert = parse.translate('insert: behavior_d, 1 3')
+    db.insert(*insert)
+    insert = parse.translate('insert: behavior_d, 2 3')
+    db.insert(*insert)
+    insert = parse.translate('insert: behavior_d, 23 5')
+    db.insert(*insert)
+    select = parse.translate('select: behavior_d, a=1')
+    select = parse.translate('select: behavior_d, b=5')
+    print(db.select(*select))
